2ad14368aa092295e550caf71c73f3cd604c1d37
doug gilmore <d.gilmore@mac.com>
2025-03-29 00:16:34 -0500

    fixed footer

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -10,8 +10,8 @@
    _..`--'_..-_/  /--'_.' ,'                repo:  https://github.com/beneliath/DNR
   (il),-''  (li),'  ((!.-'                 title:  DNR - deploy & report
                                          version:  0.0.1
- Genesis 49:10 ... Revelation 5:5      timestamp:  2025-03-28 23:06:10
-        Do you see Him?
+Genesis 49:9,10 ... Revelation 5:5     timestamp:  2025-03-29 00:15:58
+         Do you see Him?
     </pre>
     </div>
 </footer>
