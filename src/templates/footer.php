9c8e43f1f5de501dba2c29c6d70673387cfd94dc
doug gilmore <d.gilmore@mac.com>
2025-04-05 11:55:09 -0500

    rolling: footer.php

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,8 +1,8 @@
 <footer>
-    <p style="opacity: 0.2; display: inline;">&copy; <?php echo date("Y"); ?> beneliath&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Shalom in Messiah Ministries&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</p><a href="logout.php" style="text-decoration: none; color: var(--link-color);">Logout (<?php echo htmlspecialchars($_SESSION['username']); ?>)</a>
+    <p style="opacity: 0.2; display: inline;">&copy; <?php echo date("Y"); ?> beneliath&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</p><a href="logout.php" style="text-decoration: none; color: var(--link-color);">Logout (<?php echo htmlspecialchars($_SESSION['username']); ?>)</a>
     
     <!-- ASCII Art Container -->
-    <div class="ascii-art-container" style="opacity: 0.2; font-size: 0.8em;">
+    <div class="ascii-art-container" style="opacity: 0.2; font-size: 1.0em;">
     <pre style="font-size: 0.8em;">
      ("`-''-/").___..--''"`-.
      `6_ 6  )   `-.  (     ).`-.__.`)
@@ -10,7 +10,7 @@
    _..`--'_..-_/  /--'_.' ,'                repo:  https://github.com/beneliath/DNR
   (il),-''  (li),'  ((!.-'                 title:  DNR - deploy & report
                                          version:  0.0.1
-Genesis 49:9,10 ... Revelation 5:5     timestamp:  2025-03-29 00:15:58
+Genesis 49:9,10 ... Revelation 5:5     timestamp:  2025-04-05 11:54:55
          Do you see Him?
     </pre>
     </div>
